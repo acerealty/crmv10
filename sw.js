7cf6ea8a5bd1fcc180247e0aef60c200e388389d
acerealty <sahishnaik06@gmail.com>
2025-12-12 17:52:48 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('ace-crm-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './ace-logo-192.png',
-        './ace-logo-512.png',
-        './manifest.webmanifest'
-      ]);
-    })
+// sw.js (for GitHub Pages project folder)
+const CACHE = 'acecrm-static-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{})
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('activate', evt => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+  const req = evt.request;
+  if (req.mode === 'navigate') {
+    evt.respondWith(fetch(req).catch(()=> caches.match('./index.html')));
+    return;
+  }
+  evt.respondWith(caches.match(req).then(res => res || fetch(req)));
 });
